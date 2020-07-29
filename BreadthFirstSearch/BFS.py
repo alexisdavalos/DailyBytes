a62d706d888798b07aced6be10633057fd36a747
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-07-28 23:22:15 -0400

    updated BFS, added DFS

--- BreadthFirstSearch/BFS.py
+++ BreadthFirstSearch/BFS.py
@@ -1,26 +1,45 @@
 class Node:
-    def __init__(self, name):
+    def __init__(self, value):
         self.children = []
-        self.name = name
+        self.value = value
+   
+    def __repr__(self):
+        return f'({self.value})'
 
-    def addChild(self, name):
-        self.children.append(Node(name))
+    def addChild(self, value):
+        self.children.append(Node(value))
         return self
 
-    def breadthFirstSearch(self, array):
+    # O(n + e) time | O(n) space
+    def breadthFirstSearch(self, array=[]):
+        # initialize a queue with root node | FIFO
         q = []
         q.append(self)
+        
+        # loop while the queue is not empty
         while len(q) > 0:
+            # assign cur pointer to first node to be dequeued
             cur = q.pop(0)
-            array.append(cur.name)
+            # add that node's value to output array 
+            array.append(cur)
+            # if node has children, add them to queue
             for child in cur.children:
                 q.append(child)
-        return array
+        # return initial array
+        return f'Tree Contains: {array}'
 
 # Driver Code
 
 Tree = Node(1)
+Tree.addChild(2)
+Tree.children[0].addChild(3)
+Tree.addChild(4)
+Tree.children[0].addChild(23)
+Tree.addChild(4)
+Tree.children[0].addChild(33)
+Tree.addChild(4)
+Tree.addChild(5)
+Tree.children[1].addChild(7)
 for i in range(2,10):
     Tree.addChild(i)
-arr = []
-print(Tree.breadthFirstSearch(arr))
+print(Tree.breadthFirstSearch())
---
+++ DepthFirstSearch/DFS.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+    
+    def __repr__(self):
+        return f'Node({self.name})'
+    
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array=[]):
+        # Add current node to array
+        array.append(self)
+        # Traverse throguh all the children recursively
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
+# Driver Code
+
+Tree = Node(1)
+Tree.addChild(33)
+Tree.addChild(33)
+Tree.children[0].addChild(44)
+Tree.children[0].addChild(55)
+for i in range(2,10):
+    Tree.addChild(i)
+
+print(Tree.depthFirstSearch())
