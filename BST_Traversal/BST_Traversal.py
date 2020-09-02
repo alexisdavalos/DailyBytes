38fb7bf6d8ef3e98e37de6df53079c145ddbb527
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-09-02 23:36:21 +0200

    BST Traversal

--- BST_Traversal/BST_Traversal.py
+++ BST_Traversal/BST_Traversal.py
@@ -4,36 +4,67 @@ class BST:
         self.left = None
         self.right = None
 
-    def inOrderTraverse(self):
-		# Store Tree Nodes While Traversing
-		array = []
-		# Recursively traverse nodes 
-        if self is not None:
-            inOrderTraverse(self.left, array)
-            array.append(self.value)
-            inOrderTraverse(self.right, array)
-		# Return populated array
+    def __repr__(self):
+        return f'{self.inOrderTraverse(self, array=[])}'
+
+    def insert(self, value):
+        # Given value is less than node's value
+        if value < self.value:
+            # Node is a leaf node, insert new node
+            if self.left is None:
+                self.left = BST(value)
+        # Otherwise keep traversing the tree
+            else:
+                self.left.insert(value)
+        # Given value is greater than node's value
+        elif value > self.value:
+            # Node is a leaf node, insert new node
+            if self.right is None:
+                self.right = BST(value)
+            # Otherwise keep traversing the tree
+            else:
+                self.right.insert(value)
+
+    def inOrderTraverse(self, tree, array=[]):
+        # Recursively traverse nodes
+        if tree is not None:
+            self.inOrderTraverse(tree.left, array)
+            array.append(tree.value)
+            self.inOrderTraverse(tree.right, array)
+            # Return populated array
         return array
 
-    def preOrderTraverse(self):
-		# Store Tree Nodes While Traversing
-		array = []
-		# Recursively traverse nodes 
-        if self is not None:
-            array.append(self.value)
-            preOrderTraverse(self.left, array)
-            preOrderTraverse(self.right, array)
-		# Return populated array
+    def preOrderTraverse(self, tree, array=[]):
+        # Recursively traverse nodes
+        if tree is not None:
+            array.append(tree.value)
+            self.preOrderTraverse(tree.left, array)
+            self.preOrderTraverse(tree.right, array)
+            # Return populated array
+        return array
+
+    def postOrderTraverse(self, tree, array=[]):
+        # Recursively traverse nodes
+        if tree is not None:
+            self.postOrderTraverse(tree.left, array)
+            self.postOrderTraverse(tree.right, array)
+            array.append(tree.value)
+            # Return populated array
         return array
 
 
-    def postOrderTraverse(self):
-		# Store Tree Nodes While Traversing
-		array = []
-		# Recursively traverse nodes 
-        if self is not None:
-            postOrderTraverse(self.left, array)
-            postOrderTraverse(self.right, array)
-            array.append(self.value)
-		# Return populated array
-        return array
+# Driver Code
+# Create BST
+Tree = BST(1)
+Tree.insert(2)
+Tree.insert(5)
+Tree.insert(5)
+Tree.insert(10)
+Tree.insert(15)
+Tree.insert(22)
+
+
+# Traversals
+print(Tree.inOrderTraverse(Tree))
+print(Tree.preOrderTraverse(Tree))
+print(Tree.postOrderTraverse(Tree))
