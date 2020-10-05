ca362a5e2ff137a2acafa287085c920b5a9c5dc5
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-10-05 10:26:21 +0200

    cleanup

--- BST_Traversal/BST_Traversal.py
+++ BST_Traversal/BST_Traversal.py
@@ -29,35 +29,19 @@ class BST:
                 self.right.insert(value)
 
     def inOrderTraverse(self, tree, array=[]):
-        # Recursively traverse nodes
-        if tree is not None:
-            self.inOrderTraverse(tree.left, array)
-            array.append(tree.value)
-            self.inOrderTraverse(tree.right, array)
-            # Return populated array
+        # Your Code Here
         return array
 
     def preOrderTraverse(self, tree, array=[]):
-        # Recursively traverse nodes
-        if tree is not None:
-            array.append(tree.value)
-            self.preOrderTraverse(tree.left, array)
-            self.preOrderTraverse(tree.right, array)
-            # Return populated array
+        # Your Code Here
         return array
 
     def postOrderTraverse(self, tree, array=[]):
-        # Recursively traverse nodes
-        if tree is not None:
-            self.postOrderTraverse(tree.left, array)
-            self.postOrderTraverse(tree.right, array)
-            array.append(tree.value)
-            # Return populated array
+        # Your Code Here
         return array
 
 
-# Driver Code
-# Create BST
+# Test Code
 Tree = BST(1)
 Tree.insert(2)
 Tree.insert(5)
